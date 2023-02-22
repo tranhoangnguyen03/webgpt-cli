72831613114582239f5a41632044eb73b70d2d5f
mukul <mukulpatnaik@gmail.com>
2023-02-22 05:28:01 -0700

    adding flags

--- gpt.py
+++ gpt.py
@@ -1,12 +1,23 @@
-# standard GPT-3 API call
-
+import argparse
 import openai
-import sys
 import os
 
-prompt = sys.argv[1]
+# Create the argument parser
+parser = argparse.ArgumentParser(description='Use OpenAI GPT-3 API to generate text.')
+parser.add_argument('prompt', type=str, help='the prompt to generate text from')
+parser.add_argument('-m', '--model', type=str, default='text-davinci-003', help='the GPT-3 model to use')
+parser.add_argument('-t', '--temperature', type=float, default=0.8, help='the sampling temperature')
+parser.add_argument('-max', '--max-tokens', type=int, default=800, help='the maximum number of tokens to generate')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Set the OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
-r = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.8, max_tokens=800)
+
+# Call the OpenAI API
+r = openai.Completion.create(model=args.model, prompt=args.prompt, temperature=args.temperature, max_tokens=args.max_tokens)
 response = r.choices[0]['text']
 
-print('\n'+response.strip("\n")+'\n')
+# Print the generated text
+print('\n' + response.strip("\n") + '\n')
